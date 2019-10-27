aeaee60522015d3bd664c80c782ea08fa6ccd87e
kurosawa4434 <kurosawa4434@gmail.com>
2019-10-28 01:17:34 +0900

    Update my_solution.py

--- verification/my_solution.py
+++ verification/my_solution.py
@@ -13,18 +13,15 @@ werewolf
 skeleton
 frankenstein
 '''
-m_list = [ms for ms in MONSTERS.splitlines() if ms]
 
 
 def halloween_monsters(spell):
 
-    ct_spell = Counter(spell)
-    tgt_monsters = [m for m in m_list if not Counter(m) - ct_spell]
     maximum_monsters = []
 
     def search(rest_spell, rest_monsters, monsters, num=0, max_num=0):
 
-        if not rest_spell or not rest_monsters:
+        if not (rest_spell and rest_monsters):
             if num > max_num:
                 maximum_monsters.clear()
             if num >= max_num:
@@ -33,19 +30,19 @@ def halloween_monsters(spell):
 
         name_monster = rest_monsters[0]
         ct_monster = Counter(name_monster)
-
         new_spell = deepcopy(rest_spell)
         new_monsters = list(monsters)
-        max_num = search(new_spell, rest_monsters[1:], new_monsters, num, max_num)
-        while not ct_monster - new_spell:
+        while True:
+            max_num = search(new_spell, rest_monsters[1:], new_monsters, num, max_num)
+            if ct_monster - new_spell:
+                break
             new_spell -= ct_monster
             new_monsters.append(name_monster)
             num += 1
-            max_num = search(new_spell, rest_monsters[1:], new_monsters, num, max_num)
 
         return max_num
 
-    result = search(ct_spell, tgt_monsters, [])
+    result = search(Counter(spell), MONSTERS.split(), [])
     return result, maximum_monsters and maximum_monsters[0]
 
 
